8fc7e8675cec57ad179dffb19521b2196bf6cf79
xasano5o <abdurazakovxasan_1oo@mail.ru>
2022-04-02 12:52:43 +0500

    first commit

---
+++ cllas/cllas-1.js
@@ -0,0 +1,24 @@
+// class Player {
+// 	constructor(name, age, height, weight) {
+// this.name=name ;
+// this.age=age ;
+// this.height=height ;
+// this.weight=weight ;
+// 	}
+
+	
+// 	getAge() {
+// 	return `${this.name} is age ${this.age}`
+// 	}
+
+// 	getHeight() {
+// 	return `${this.name} is ${this.height}cm`
+// 	}
+
+
+var arr =new Array ('WebBrainAcademy' ,18 ,javaScript ,true )
+
+
+var arr2= []
+
+console.log(arr);
